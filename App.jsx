762703e97e45ae768303aff349ed2422ceae3d4a
Bowbosh22 <bowbosh@gmail.com>
2026-09-15 20:51:55 +0100

    Fix duplicate import and render WhatsAppFloat component

--- App.jsx
+++ App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 
 import Navbar from './components/Navbar'
 import WhatsAppFloat from './components/WhatsAppFloat'
+import WhatsAppFloat from './components/WhatsAppFloat'
 import Home from './pages/Home'
 import Chambres from './pages/Chambres'
 import ChambreDetails from './pages/ChambreDetails'
@@ -25,6 +26,7 @@ function AppContent() {
         <Route path="/chambres/:slug" element={<ChambreDetails />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+            <WhatsAppFloat />
       <WhatsAppFloat />
     </>
   )
